3d552ce31d3d37de563ea8fb18f9e71835986959
kteinikgerman-cpu <kteinikgerman@gmail.com>
2026-02-11 01:50:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 
 # Токен из переменных окружения (безопасность!)
-BOT_TOKEN = "7564138089:AAFZm0WjZ_EObBCUb8KXxCmKKxk_YpZD5VM"
+BOT_TOKEN = "7564138089:AAFZm0WjZ_EObBCUb8KXxCmKKxk_YpZD5VM "
 EXCEL_FILE = 'base.xlsx'  # Файл будет загружен на GitHub
 WEB_APP_URL = 'https://kteinikgerman-cpu.github.io/planogram-bot/webapp.html'
 
